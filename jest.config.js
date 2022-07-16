4811928d8efb5b017dd2b03d3a81903e71c92257
Patrik Kaura <patrikkaura@gmail.com>
2022-07-16 21:01:25 +0200

    feat: initialize github actions

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,24 @@
 // jest.config.js
-const nextJest = require('next/jest');
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
-  dir: './',
+  dir: "./",
 });
 
 const customJestConfig = {
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    "^@components/(.*)$": "<rootDir>/components/$1",
+    "^@graphql/(.*)$": "<rootDir>/graphql/$1",
+    "^@styles/(.*)$": "<rootDir>/styles/$1",
+    "^@generated/(.*)$": "<rootDir>/generated/$1",
+    "^@models/(.*)$": "<rootDir>/models/$1",
+    "^@hooks/(.*)$": "<rootDir>/hooks/$1",
+    "^types/(.*)$": "<rootDir>/types/$1",
+    "^@config$": "<rootDir>/config/index",
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
